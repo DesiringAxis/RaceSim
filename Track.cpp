c9cd4f0b6f28f0b9db69b91c1fd5d217dc37a5bc
CrunchyTACO36 <njr5519@psu.edu>
2024-04-11 17:43:26 -0400

    Track files

--- Track.cpp
+++ Track.cpp
@@ -1,1 +1,32 @@
-//Hold all track information
+#include "Track.h"
+
+// Constructor
+Track::Track(const std::string& name, const std::string& location, int turns, int straights)
+    : name(name), location(location), turns(turns), straights(straights) {}
+
+std::string Track::getName() const {
+    return name;
+}
+
+std::string Track::getLocation() const{
+    return location;
+}
+
+int Track::getTurns() const{
+    return turns;
+}
+
+int Track::getStraights() const{
+    return straights;
+}
+
+// Initialize specific tracks, constructs object inside container
+std::vector<Track> intitializeTracks() {
+    std::vector<Track> tracks;
+    tracks.emplace_back("Silverstone", "UK", 18, 6);
+    tracks.emplace_back("Monza", "Italy", 11, 4);
+    tracks.emplace_back("Spa-Francorchamps", "Belgium", 19, 10);
+    tracks.emplace_back("Suzuka", "Japan", 18, 5);
+    tracks.emplace_back("Monaco", "Monaco", 19, 3);
+    return tracks;
+}
--- Track.h
+++ Track.h
@@ -1,5 +1,29 @@
-//header for track
+//Hold all track information
 #ifndef TRACK_H
 #define TRACK_H
+
+#include <string>
+#include <vector>
+
+class Track {
+private:
+    std::string name;       // Track name
+    std::string location;   // Track location
+    int turns;              // Number of turns
+    int straights;          // Number of straights
+
+public:
+    // Initialize track with name, location, # of turns, # of straights
+    Track(const std::string& name, const std::string& location, int turns, int straights);
+
+    std::string getName() const; // Gets track name
+    std::string getLocation() const; // Gets track location
+    int getTurns() const; // Gets number of turns
+    int getStraights() const; // Gets number of straights
+
+};
+
+// Initialization to retrieve tracks
+std::vector<Track> intitializeTracks();
 
 #endif // TRACK_H
